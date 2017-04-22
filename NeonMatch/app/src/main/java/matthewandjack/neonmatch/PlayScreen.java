431eacb0e3f757ee4a6477d524888d30f4ba3442
HelloMatthewMurphy <hellomatthewmurphy@gmail.com>
2017-04-22 20:54:47 +0100

    Added play screen

--- NeonMatch/app/src/main/java/matthewandjack/neonmatch/PlayScreen.java
+++ NeonMatch/app/src/main/java/matthewandjack/neonmatch/PlayScreen.java
@@ -4,19 +4,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 /**
  * Created by user on 22/04/2017.
  */
 
 public class PlayScreen extends AppCompatActivity {
-    
+    private boolean red = false;
+    private boolean blue = false;
+    private boolean green = false;
+    private boolean yellow = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_play_screen);
+    }
+
+    public void redPressed(){
+        red = true;
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("RED");
+    }
+
+    public void bluePressed(){
+        blue = true;
+    }
+
+    public void greenPressed(){
+        green = true;
+    }
+
+    public void yellowPressed(){
+        yellow = true;
     }
 }
